e62d6e770d553048aaa33fe1df1de33dfccba9c2
KosarevPV <pavel999kosarev@gmail.com>
2023-07-03 15:07:44 +0300

    add logging

---
+++ lesson_5/logs/server_log_config.py
@@ -0,0 +1,30 @@
+import logging.handlers
+import os
+import sys
+
+from lesson_5.common.variables import LOGGING_LEVEL
+
+
+PATH = os.path.dirname(__file__)
+PATH = os.path.join(PATH, 'log_files\\server.log')
+
+SERVER_FORMATTER = logging.Formatter('%(asctime)s %(levelname)s %(filename)s %(message)s')
+
+STREAM_HANDLER = logging.StreamHandler(sys.stderr)
+STREAM_HANDLER.setFormatter(SERVER_FORMATTER)
+STREAM_HANDLER.setLevel(logging.ERROR)
+
+LOG_FILE = logging.handlers.TimedRotatingFileHandler(PATH, encoding='utf8', interval=1, when='D')
+LOG_FILE.setFormatter(SERVER_FORMATTER)
+
+LOGGER = logging.getLogger('server')
+LOGGER.addHandler(STREAM_HANDLER)
+LOGGER.addHandler(LOG_FILE)
+LOGGER.setLevel(LOGGING_LEVEL)
+
+
+if __name__ == '__main__':
+    LOGGER.critical('Критическая ошибка')
+    LOGGER.error('Ошибка')
+    LOGGER.debug('Отладочная информация')
+    LOGGER.info('Информационное сообщение')
